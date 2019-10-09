21251252667064d80466161d432bded61a4f4689
Drew Letcher <drew@drewletcher.net>
2019-10-09 09:01:09 -0500

    moved notify to storage-node project

--- src/api/index.js
+++ src/api/index.js
@@ -7,11 +7,9 @@ const express = require('express');
 
 const status = require('./status');
 const settings = require('./settings');
-const notify = require('./notify');
 
 /* sub routes */
 const router = express.Router();
 router.use('/status', status);
 router.use('/settings', settings);
-router.use('/notify', notify);
 module.exports = router;
